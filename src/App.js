77d80f7fcf8ceb03703c7d9b5b8d283b14068000
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-06-11 21:08:29 -0300

    actualizacion de proyecto con carrousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import About from "./components/about";
-import Card from "./components/cards";
 import Header from "./components/header";
+import Footer from "./components/footer";
+import Example from "./components/cards";
 
 function App() {
   return (
     <div>
       <Header />
       <About />
-      <Card />
+      <Footer />
+      <Example />
     
     </div>
   
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import "./style.css"
+import "./header.css"
 
 
 function Header() {
@@ -11,8 +11,3 @@ function Header() {
 
 export default Header
 
-/*
-<a href="https://fontmeme.com/netflix-font/">
-<img src="https://fontmeme.com/permalink/230609/5f6f22563c200c63308baab8a20810f7.png" alt="netflix-font" border="0" />
-</a>
-*/
